3afcb3e07eaea9392dfca99893c7e25cccd5f3b7
Max <maxeust@gmail.com>
2023-01-04 22:54:21 +0100

    spacefile merge conflict

--- main.py
+++ main.py
@@ -1,63 +1,67 @@
 import os
 import json
 import requests
+import base64
+from deta import Deta
 from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+deta = Deta()
+
+photos = deta.Drive("generations")
 
 bot_key = os.getenv("TELEGRAM") or ""
 
 bot_url = "https://api.telegram.org/bot" + bot_key + "/"
 
+open_ai_url = "https://api.openai.com/v1/images/generations"
 
-def build_response(user_input):
-    preamble = "Space takes: " + '\n\n' + user_input + '\n\n'
-    gap = "Space gives: " + '\n\n' + user_input[::-1] + '\n\n'
-    response = """
-I, DETA Space, dreamt I was a friendly personal computer, easily shipping apps for developers 
-with ideas which were grounded. I could easily bring these ideas past the spirit of gravity 
-and above large and unfriendly clouds. Everything about me worked, and I didn't need configuration.
-                
-Then, I awoke, and realized again I was DETA Space, a cloud unlike any personal computer. 
-I was natively integrated with the Intergalactic Planetary Network. I could be scheduled 
-to work for a human even after they shut down their terminal. My programs were 'liquid' 
-as never before, as personal apps became collaborative, and collaborative apps personal, 
-at the flick of a switch.
-                
-Now I do not know whether I am a personal computer, which dreamt I was above the 
-clouds, or I am a cloud, above all personal computers. Between a personal computer 
-and a cloud there is a blurring distinction."""
-    return preamble + gap + response
-
-
-def preprocess(data):
-    user_input = data['message']['text']
-    response_text = build_response(user_input)
-    user_identity = data['message']['chat']['id']
-    json_response = {
-        "chat_id": user_identity,
-        "text": response_text,
+def get_image_from_prompt(prompt):
+    open_ai_data = {
+        "prompt": prompt,
+        "n": 1,
+        "size": "512x512",
+        "response_format": "b64_json"
     }
-    return json_response
+    auth_header = "Bearer " + os.getenv("OPEN_AI")
+    headers = {"Content-Type": "application/json", "Authorization": auth_header}
+    response = requests.post(open_ai_url, json=open_ai_data, headers=headers).json()
+    if not "error" in response:
+        return {"b64img": response["data"][0]["b64_json"], "created": response["created"]}
+    return {"error": response["error"]["message"]}
 
+def save_and_send_img(b64img, chat_id, prompt, timestamp):
+    image_data = base64.b64decode(b64img)
+    filename = f"{timestamp} - {prompt}.png"
+    photos.put(filename, image_data)
+    photo_payload = {"photo": image_data}
+    message_url = bot_url + f"sendPhoto?chat_id={chat_id}&caption={prompt}"
+    response = requests.post(message_url, files=photo_payload).json()
+    return {"chat_id": chat_id, "caption": prompt}
 
-def send_message(preprocessed_data):  
-    message_url = bot_url + 'sendMessage'
-    requests.post(message_url, json=preprocessed_data)
-
+def send_error(chat_id, error_message):
+    message_url = bot_url + f"sendMessage"
+    payload = {"text": error_message, "chat_id": chat_id}
+    response = requests.post(message_url, json=payload).json()
+    return response
 
 @app.post("/open")
 async def http_handler(request: Request):
     incoming_data = await request.json()
-    answer_data = preprocess(incoming_data)
-    send_message(answer_data)
-    return {}
+    prompt = incoming_data['message']['text']
+    user_identity = incoming_data['message']['chat']['id']
+    open_ai_resp = get_image_from_prompt(prompt)
+    if "b64img" in open_ai_resp:
+        return save_and_send_img(open_ai_resp["b64img"], user_identity, prompt, open_ai_resp["created"])
+    if "error" in open_ai_resp:
+        return send_error(user_identity, open_ai_resp["error"])
+    return send_error(user_identity, "Unknown error, lol, handling coming soon")
 
 
 @app.get("/")
 def url_setter(request: Request):
-    prog_url = os.getenv("DETA_SPACE_APP_HOSTNAME") or ""
+    prog_url = os.getenv("DETA_SPACE_APP_HOSTNAME") or "87df-2003-d2-972a-3a5b-1dcf-b2af-f6e8-d9d4.ngrok.io"
     set_url = bot_url + "setWebHook?url=" + "https://" + prog_url +"/open"
     resp = requests.get(set_url)
     return resp.json()
